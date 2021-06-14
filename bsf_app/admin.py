ffcf852b2f27fb697a7300b7381055d60a12a715
icarus-0 <naman.chandel_cs17@gla.ac.in>
2021-06-14 15:56:46 +0530

    match Database added

--- bsf_app/admin.py
+++ bsf_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from bsf_app.models import *
 
+
+admin.site.register(Match)
 # Register your models here.
--- bsf_app/models.py
+++ bsf_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Match(models.Model):
+    match_pk = models.AutoField(primary_key=True) 
+    match_id = models.CharField(max_length=100)
+    match_name =  models.CharField(max_length=150)
+    match_type =  models.CharField(max_length=150)
+    match_details = models.JSONField()
+    
+    def __str__(self):
+        return self.match_name+'  '+self.match_id 
