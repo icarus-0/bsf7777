1408bf7d446c43e8e285b3b0183042d1a1d07a07
icarus-0 <naman.chandel_cs17@gla.ac.in>
2021-06-27 15:19:11 +0530

    laghai khai betting done

--- bsf_app/admin.py
+++ bsf_app/admin.py
@@ -5,4 +5,6 @@ from bsf_app.models import *
 admin.site.register(Match)
 admin.site.register(Match_Score)
 admin.site.register(BettingDetail)
+admin.site.register(LaghaiKhaliBetDetail)
+admin.site.register(UserCoins)
 # Register your models here.
